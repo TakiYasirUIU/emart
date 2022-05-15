67e8b95ec1b8b0a37b9b0753e69d990ef1371eba
TakiYasirUIU <yasirtaki1995@gmail.com>
2022-05-15 22:39:27 +0600

    navbar update

--- home.php
+++ home.php
@@ -21,7 +21,7 @@
         <div class="right_container rounded">
             
             <div class="bottom">
-              <nav class="navbar navbar-expand-lg navbar-light bg-light">
+              <nav class="navbar navbar-expand-lg navbar-light">
                 <div class="container-top">
                   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
